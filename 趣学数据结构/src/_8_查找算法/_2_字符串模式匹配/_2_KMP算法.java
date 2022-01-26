e9d5f2ecf226bbcaf4b0fe80a3a1fc5be104b877
caiwillie <caiwl@shinemo.com>
2022-01-26 17:36:06 +0800

    更新

--- 趣学数据结构/src/_8_查找算法/_2_字符串模式匹配/_2_KMP算法.java
+++ 趣学数据结构/src/_8_查找算法/_2_字符串模式匹配/_2_KMP算法.java
@@ -1,15 +1,54 @@
 package _8_查找算法._2_字符串模式匹配;
 
+import sun.text.normalizer.UCharacter;
+
 public class _2_KMP算法 {
 
-    static void getNext(String $t) {
-        int length = $t.length();
-        char[] t = new char[length + 1];
-        System.arraycopy($t.toCharArray(), 0, t, 1, length);
-        int[] next = new int[length + 1];
 
-        next[0] = -1;
-        int k = -1;
+
+
+
+
+    static int sl = 0;
+    static int tl = 0;
+    static char[] s = null;
+    static char[] t = null;
+    static int[] next = null;
+
+    static int kmp(String $s, String $t) {
+        // 处理数据
+        sl = $s.length();
+        tl = $t.length();
+        s = new char[sl + 1];
+        t = new char[tl + 1];
+        next = new int[tl + 1];
+        System.arraycopy($t.toCharArray(), 0, t, 1, tl);
+        System.arraycopy($s.toCharArray(), 0, s, 1, sl);
+        getNext($t);
+        int i, j = 1;
+        // s=abaabaabeca, t=abaabe: i = 1, j = 1
+        for (i = 1; i <= sl && j <= tl; i++) {
+            while(true) {
+                if(j == 0 || s[i] == t[j]) {
+                    j++;
+                    break;
+                } else {
+                    j = next[j];
+                }
+            }
+        }
+
+        if(j > tl) {
+            return i - tl;
+        } else
+            return -1;
+    }
+
+
+    static void getNext(String $t) {
+
+        int k = next[0] = -1;
+        // k 代表 i - 1时的最大长度
         // a,b -> k = 0(next[1]), a[1], a[2]
         // a,a -> k = 0(next[1]), a[1], a[2]
         // aa,a -> k = 1(next[2]), a[2], a[3] 比较
@@ -17,8 +56,7 @@ public class _2_KMP算法 {
         // i = 2, k 代表 i = 1 时的长度
         // i = 3, k = 1
 
-        // k 代表 i - 1时的最大长度
-        for (int i = 1; i <= length; i++) {
+        for (int i = 1; i <= tl; i++) {
             while(true) {
                 if(k == -1 || t[k + 1] == t[i]) {
                     next[i] = ++k;
@@ -30,12 +68,9 @@ public class _2_KMP算法 {
         }
     }
 
-
     public static void main(String[] args) {
-        getNext("abba");
-        return;
-
-
+        int kmp = kmp("abaabaabeca", "abaab");
+        System.out.println(kmp);
     }
 
 }
---
+++ 趣学数据结构/src/_8_查找算法/_3_二叉查找树/BST.java
@@ -0,0 +1,114 @@
+package _8_查找算法._3_二叉查找树;
+
+public class BST {
+
+    /*
+    BST的结构
+    * */
+    static class BSTree {
+        int value;
+        BSTree left;
+        BSTree right;
+
+    }
+
+    /*
+    F....
+        :
+        :...p
+
+    * */
+
+
+    /*
+    查找
+    * */
+    BSTree search(BSTree t, int value) {
+        BSTree p = t;
+        while(p != null) {
+            if(p.value == value) {
+                break;
+            }
+
+            if (p.value > value) {
+                // 当前节点比目标值大，在左子树中找
+                p = p.left;
+            } else {
+                // 当前节点比目标值小，在右子树中找
+                p = p.right;
+            }
+
+        }
+
+        return p;
+    }
+
+
+    /**
+     * 删除
+     *
+     * @param t     the t
+     * @param value the value
+     * O(n) = logn, 主要就是查找的时间以及找前驱和后继的时间
+     */
+    void delete(BSTree t, int value) {
+        BSTree p = t;
+        BSTree f = null, q = null, s = null;
+        while(p != null) {
+            if(p.value == value) {
+                break;
+            }
+            // f 为 p 的父节点
+            f = p;
+            if (p.value > value) {
+                // 当前节点比目标值大，在左子树中找
+                p = p.left;
+            } else {
+                // 当前节点比目标值小，在右子树中找
+                p = p.right;
+            }
+        }
+
+        if(p == null) return;
+
+        if (p.left != null && p.right != null) {
+            // 左右子树都不为空
+            q = p;
+            s = p.left;
+            while(s.right != null) {
+                // 在左子树中找前驱
+                q = s;
+                s = s.right;
+            }
+
+            //然后修改q节点的指针
+            if(q != p) {
+                q.right = s.left;
+            } else {
+                q.left = s.left;
+            }
+
+            // 修改s的左右指针
+            s.left = p.left;
+            s.right = p.right;
+
+        } else {
+            // 左右子树至少有一个为空，已经包含了左右子树都为空
+            if(p.left == null) {
+                s = p.right;
+            } else {
+                s = p.left;
+            }
+        }
+
+        if(f == null) {
+            t = s;
+        } else if (p == f.left) {
+            f.left = s;
+        } else {
+            f.right = s;
+        }
+    }
+    
+
+}
