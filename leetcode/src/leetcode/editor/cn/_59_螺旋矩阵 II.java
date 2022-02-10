d340ba92ba0bfbf634022d5e6f82467c91e4801c
caiwillie <caiwl@shinemo.com>
2022-02-10 20:07:58 +0800

    更新代码

--- leetcode/src/leetcode/editor/cn/_59_螺旋矩阵 II.java
+++ leetcode/src/leetcode/editor/cn/_59_螺旋矩阵 II.java
@@ -39,15 +39,19 @@ class _59_螺旋矩阵II {
             int t = 0, l = 0, r = n - 1, b = n - 1;
             int[][] mat = new int[n][n];
             int num = 1, tar = n * n;
-            while (num <= tar) {
+            while (true) {
                 for (int i = l; i <= r; i++) mat[t][i] = num++; // left to right.
                 t++;
+                if(t > b) break;
                 for (int i = t; i <= b; i++) mat[i][r] = num++; // top to bottom.
                 r--;
+                if(l > r) break;
                 for (int i = r; i >= l; i--) mat[b][i] = num++; // right to left.
                 b--;
+                if(t > b) break;
                 for (int i = b; i >= t; i--) mat[i][l] = num++; // bottom to top.
                 l++;
+                if(l > r) break;
             }
             return mat;
         }
