925d6ae8c3500fee3d0f21c164b1a621db444c14
caiwillie <caiwl@shinemo.com>
2022-02-10 10:53:52 +0800

    更新代码

---
+++ leetcode/src/leetcode/editor/cn/_400_第 N 位数字.java
@@ -0,0 +1,46 @@
+package leetcode.editor.cn;
+
+//给你一个整数 n ，请你在无限的整数序列 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...] 中找出并返回第 n 位上的数字。
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 3
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 11
+//输出：0
+//解释：第 11 位数字在序列 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... 里是 0 ，它是 10 的一部分。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 2³¹ - 1 
+// 
+// Related Topics 数学 二分查找 👍 294 👎 0
+
+class _400_第N位数字 {
+    public static void main(String[] args) {
+        Solution solution = new _400_第N位数字().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int findNthDigit(int n) {
+            return 0;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
