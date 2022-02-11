9b872ebab0ac7b69aba3411b7fc9cb403e635dae
caiwillie <caiwl@shinemo.com>
2022-02-11 16:16:19 +0800

    更新代码

---
+++ leetcode/src/leetcode/editor/cn/_673_最长递增子序列的个数.java
@@ -0,0 +1,51 @@
+package leetcode.editor.cn;
+
+//给定一个未排序的整数数组 nums ， 返回最长递增子序列的个数 。 
+//
+// 注意 这个数列必须是 严格 递增的。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: [1,3,5,4,7]
+//输出: 2
+//解释: 有两个最长递增子序列，分别是 [1, 3, 4, 7] 和[1, 3, 5, 7]。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: [2,2,2,2,2]
+//输出: 5
+//解释: 最长递增子序列的长度是1，并且存在5个子序列的长度为1，因此输出5。
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+//
+// 
+// 1 <= nums.length <= 2000 
+// -10⁶ <= nums[i] <= 10⁶ 
+// 
+// Related Topics 树状数组 线段树 数组 动态规划 👍 545 👎 0
+
+class _673_最长递增子序列的个数 {
+    public static void main(String[] args) {
+        Solution solution = new _673_最长递增子序列的个数().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int findNumberOfLIS(int[] nums) {
+            return 0;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
