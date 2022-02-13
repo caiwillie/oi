e48922ed29c6f75cebba999149492c5a15ededb2
caiwillie <caiwl@shinemo.com>
2022-02-13 20:24:09 +0800

    更新代码

---
+++ leetcode/src/leetcode/editor/cn/_718_最长重复子数组.java
@@ -0,0 +1,46 @@
+package leetcode.editor.cn;
+
+//给两个整数数组 nums1 和 nums2 ，返回 两个数组中 公共的 、长度最长的子数组的长度 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+//输出：3
+//解释：长度最长的公共子数组是 [3,2,1] 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+//输出：5
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums1.length, nums2.length <= 1000 
+// 0 <= nums1[i], nums2[i] <= 100 
+// 
+// Related Topics 数组 二分查找 动态规划 滑动窗口 哈希函数 滚动哈希 👍 613 👎 0
+
+class _718_最长重复子数组 {
+    public static void main(String[] args) {
+        Solution solution = new _718_最长重复子数组().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int findLength(int[] nums1, int[] nums2) {
+            return 0;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
