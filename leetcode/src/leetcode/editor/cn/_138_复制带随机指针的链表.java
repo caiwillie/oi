52e0bb2cf520a37697921bd41466637526ee3cc9
caiwillie <caiwl@shinemo.com>
2022-02-14 19:30:14 +0800

    更新代码

--- leetcode/src/leetcode/editor/cn/_138_复制带随机指针的链表.java
+++ leetcode/src/leetcode/editor/cn/_138_复制带随机指针的链表.java
@@ -64,7 +64,7 @@ class _138_复制带随机指针的链表 {
         Solution solution = new _138_复制带随机指针的链表().new Solution();
 
     }
-//leetcode submit region begin(Prohibit modification and deletion)
+    //leetcode submit region begin(Prohibit modification and deletion)
 /*
 // Definition for a Node.
 class Node {
@@ -82,7 +82,38 @@ class Node {
 
     class Solution {
         public Node copyRandomList(Node head) {
-            return null;
+            if (head == null) {
+                return null;
+            }
+            Node p = head;
+            //第一步，在每个原节点后面创建一个新节点
+            //1->1'->2->2'->3->3'
+            while (p != null) {
+                Node newNode = new Node(p.val);
+                newNode.next = p.next;
+                p.next = newNode;
+                p = newNode.next;
+            }
+            p = head;
+            //第二步，设置新节点的随机节点
+            while (p != null) {
+                if (p.random != null) {
+                    p.next.random = p.random.next;
+                }
+                p = p.next.next;
+            }
+
+            Node dummy = new Node(-1);
+            p = head;
+            Node cur = dummy;
+            //第三步，将两个链表分离
+            while (p != null) {
+                cur.next = p.next;
+                cur = cur.next;
+                p.next = cur.next;
+                p = p.next;
+            }
+            return dummy.next;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
