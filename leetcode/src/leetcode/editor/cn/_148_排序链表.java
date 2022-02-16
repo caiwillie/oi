42c5eb0859570565173ed767d87519f52a8044e1
caiwillie <caiwl@shinemo.com>
2022-02-16 18:36:39 +0800

    更新代码

--- leetcode/src/leetcode/editor/cn/_148_排序链表.java
+++ leetcode/src/leetcode/editor/cn/_148_排序链表.java
@@ -45,7 +45,14 @@ package leetcode.editor.cn;
 class _148_排序链表 {
     public static void main(String[] args) {
         Solution solution = new _148_排序链表().new Solution();
+        // 4,2,1,3
+        ListNode _4 = new ListNode(3, null);
+        ListNode _3 = new ListNode(1, _4);
+        ListNode _2 = new ListNode(2, _3);
+        ListNode _1 = new ListNode(4, _2);
 
+        ListNode ans = solution.sortList(_1);
+        return;
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 
@@ -61,7 +68,62 @@ class _148_排序链表 {
      */
     class Solution {
         public ListNode sortList(ListNode head) {
-            return null;
+            int length = 0;
+            ListNode node = head;
+            while (node != null) {
+                length++;
+                node = node.next;
+            }
+            ListNode dummy = new ListNode(-1, head);
+            for (int subLength = 1; subLength < length; subLength = 2 * subLength) {
+                ListNode g = dummy, cur = dummy.next;
+                while (cur != null) {
+                    ListNode head1 = cur;
+                    ListNode pre = null;
+                    for (int i = 1; i <= subLength && cur != null; i++) {
+                        pre = cur;
+                        cur = cur.next;
+                    }
+                    pre.next = null;
+
+                    pre = null;
+                    ListNode head2 = cur;
+
+                    for (int i = 1; i <= subLength && cur != null; i++) {
+                        pre = cur;
+                        cur = cur.next;
+                    }
+                    if (pre != null) pre.next = null;
+
+                    ListNode merged = merge(head1, head2);
+                    g.next = merged;
+                    while (g.next != null) {
+                        g = g.next;
+                    }
+                }
+            }
+            return dummy.next;
+        }
+
+        ListNode merge(ListNode list1, ListNode list2) {
+            ListNode dummy = new ListNode(0);
+            ListNode cur = dummy, p = list1, q = list2;
+            while (p != null && q != null) {
+                if (p.val <= q.val) {
+                    cur.next = p;
+                    p = p.next;
+                } else {
+                    cur.next = q;
+                    q = q.next;
+                }
+                cur = cur.next;
+            }
+            if (p != null) {
+                cur.next = p;
+            } else if (q != null) {
+                cur.next = q;
+            }
+            return dummy.next;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
