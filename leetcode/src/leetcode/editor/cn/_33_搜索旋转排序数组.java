cc282ff3c356ec302caec4488dbe111f16ca7f3f
caiwillie <caiwl@shinemo.com>
2022-03-24 14:12:15 +0800

    更新代码

--- leetcode/src/leetcode/editor/cn/_33_搜索旋转排序数组.java
+++ leetcode/src/leetcode/editor/cn/_33_搜索旋转排序数组.java
@@ -64,7 +64,7 @@ class _33_搜索旋转排序数组 {
                     lo = mid;
                     break;
                 }
-                // 先根据 nums[mid] 与 nums[lo] 的关系判断 mid 是在左段还是右段
+                // 先根据 nums[mid] 与 nums[lo] 的关系判断 mid 是在左段还是右段，无论怎么旋转，中点和左右两端的其中一端是有序的
                 if (nums[mid] >= nums[lo]) {
                     // 再判断 target 是在 mid 的左边还是右边，从而调整左右边界 lo 和 hi
                     if (target >= nums[lo] && target < nums[mid]) {
