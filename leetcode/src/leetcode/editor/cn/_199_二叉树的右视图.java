7113848259141a1f1abdc636000167287f35d17d
caiwillie <caiwl@shinemo.com>
2022-03-29 10:13:32 +0800

    更新代码

---
+++ leetcode/src/leetcode/editor/cn/_199_二叉树的右视图.java
@@ -0,0 +1,71 @@
+package leetcode.editor.cn;
+
+//给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入: [1,2,3,null,5,null,4]
+//输出: [1,3,4]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: [1,null,3]
+//输出: [1,3]
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: []
+//输出: []
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 二叉树的节点个数的范围是 [0,100] 
+// -100 <= Node.val <= 100 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 651 👎 0
+
+import java.util.List;
+
+class _199_二叉树的右视图 {
+    public static void main(String[] args) {
+        Solution solution = new _199_二叉树的右视图().new Solution();
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public List<Integer> rightSideView(TreeNode root) {
+            return null;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
