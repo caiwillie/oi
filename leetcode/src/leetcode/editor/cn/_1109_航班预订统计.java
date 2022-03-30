eb81f37b851322e3644ede6611acd9337c6b57f4
caiwillie <caiwl@shinemo.com>
2022-03-30 16:02:33 +0800

    更新代码

--- leetcode/src/leetcode/editor/cn/_1109_航班预订统计.java
+++ leetcode/src/leetcode/editor/cn/_1109_航班预订统计.java
@@ -52,13 +52,26 @@ package leetcode.editor.cn;
 class _1109_航班预订统计 {
     public static void main(String[] args) {
         Solution solution = new _1109_航班预订统计().new Solution();
-
+        int[][] bookings = {{1, 2, 10}, {2, 3, 20}, {2, 5, 25}};
+        int[] ans = solution.corpFlightBookings(bookings, 5);
+        return;
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] corpFlightBookings(int[][] bookings, int n) {
-            return null;
+            // 求前缀和，差分思想
+            int[] counters = new int[n];
+            for (int[] booking : bookings) {
+                counters[booking[0] - 1] += booking[2];
+                if (booking[1] < n) {
+                    counters[booking[1]] -= booking[2];
+                }
+            }
+            for (int i = 1; i < n; ++i) {
+                counters[i] += counters[i - 1];
+            }
+            return counters;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
