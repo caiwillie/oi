00d12696ee61a6fed52dea8068120fc2bbcfc98a
caiwillie <caiwl@shinemo.com>
2022-04-13 15:57:39 +0800

    更新代码

--- leetcode/src/leetcode/editor/cn/_105_从前序与中序遍历序列构造二叉树.java
+++ leetcode/src/leetcode/editor/cn/_105_从前序与中序遍历序列构造二叉树.java
@@ -34,10 +34,19 @@ package leetcode.editor.cn;
 // 
 // Related Topics 树 数组 哈希表 分治 二叉树 👍 1419 👎 0
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 class _105_从前序与中序遍历序列构造二叉树 {
 
     public static void main(String[] args) {
         Solution solution = new _105_从前序与中序遍历序列构造二叉树().new Solution();
+        // preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+        int[] preorder = new int[] {3,9,20,15,7};
+        int[] inorder = new int[] {9,3,15,20,7};
+        TreeNode ans = solution.buildTree(preorder, inorder);
+        return;
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 
@@ -57,8 +66,68 @@ class _105_从前序与中序遍历序列构造二叉树 {
      * }
      */
     class Solution {
+
+        int[] c = null;
+        LinkedList<int[]> cs = new LinkedList<>();
+        LinkedList<int[]> ss = new LinkedList<>();
+
+        int[] inorder = null;
+
+        int[] preorder = null;
+
+        // 缓存结果
+        Map<Integer, TreeNode> map = new HashMap<>();
+
         public TreeNode buildTree(int[] preorder, int[] inorder) {
-            return null;
+            this.inorder = inorder;
+            this.preorder = preorder;
+
+            cs.push(new int[]{0, inorder.length - 1, 0, preorder.length - 1, -1});
+
+            while (!cs.isEmpty()) {
+                c = cs.peek();
+                if (c != ss.peek()) {
+                    int[] last = ss.peek();
+                    ss.push(c);
+                    TreeNode node = new TreeNode(preorder[c[2]]);
+                    if (last != null) {
+                        TreeNode root = map.get(last[2]);
+                        if (c[4] == 0) {
+                            root.left = node;
+                        } else if (c[4] == 1) {
+                            root.right = node;
+                        }
+                    }
+
+                    map.put(c[2], node);
+
+                    push();
+                } else {
+                    ss.pop();
+                    cs.pop();
+                }
+            }
+
+            return map.get(0);
+        }
+
+
+        void push() {
+            int is = c[0];
+            int ie = c[1];
+            int ps = c[2];
+            int pe = c[3];
+
+            int i = is;
+            while (i <= ie) {
+                if (inorder[i] == preorder[ps]) {
+                    break;
+                }
+                i++;
+            }
+            // 0 代表左节点，1代表右节点
+            if (ps + 1 <= ps + i - is) cs.push(new int[]{is, i - 1, ps + 1, ps + i - is, 0});
+            if (ps + i + 1- is <= pe) cs.push(new int[]{i + 1, ie, ps + i + 1- is, pe, 1});
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
--- leetcode/src/leetcode/editor/cn/_106_从中序与后序遍历序列构造二叉树.java
+++ leetcode/src/leetcode/editor/cn/_106_从中序与后序遍历序列构造二叉树.java
@@ -34,10 +34,18 @@ package leetcode.editor.cn;
 // 
 // Related Topics 树 数组 哈希表 分治 二叉树 👍 672 👎 0
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 class _106_从中序与后序遍历序列构造二叉树 {
     public static void main(String[] args) {
         Solution solution = new _106_从中序与后序遍历序列构造二叉树().new Solution();
-
+        // inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+        int[] inorder = {9, 3, 15, 20, 7};
+        int[] postorder = {9, 15, 7, 20, 3};
+        TreeNode ans = solution.buildTree(inorder, postorder);
+        return;
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 
@@ -58,42 +66,66 @@ class _106_从中序与后序遍历序列构造二叉树 {
      */
     class Solution {
 
+        int[] c = null;
+        LinkedList<int[]> cs = new LinkedList<>();
+        LinkedList<int[]> ss = new LinkedList<>();
+
         int[] inorder = null;
 
         int[] postorder = null;
+
+        // 缓存结果
+        Map<Integer, TreeNode> map = new HashMap<>();
 
         public TreeNode buildTree(int[] inorder, int[] postorder) {
             this.inorder = inorder;
             this.postorder = postorder;
 
-            int is = 0;
-            int ie = inorder.length - 1;
-            int ps = 0;
-            int pe = postorder.length - 1;
+            cs.push(new int[]{0, inorder.length - 1, 0, postorder.length - 1, -1});
 
-            return buildTree(is, ie, ps, pe);
-        }
+            while (!cs.isEmpty()) {
+                c = cs.peek();
+                if (c != ss.peek()) {
+                    int[] last = ss.peek();
+                    ss.push(c);
+                    TreeNode node = new TreeNode(postorder[c[3]]);
+                    if (last != null) {
+                        TreeNode root = map.get(last[3]);
+                        if (c[4] == 0) {
+                            root.left = node;
+                        } else if (c[4] == 1) {
+                            root.right = node;
+                        }
+                    }
 
-        public TreeNode buildTree(int is, int ie, int ps, int pe) {
-            if (pe < ps) {
-                return null;
+                    map.put(c[3], node);
+
+                    push();
+                } else {
+                    ss.pop();
+                    cs.pop();
+                }
             }
 
-            int root = postorder[pe];
+            return map.get(postorder.length - 1);
+        }
+
+        void push() {
+            int is = c[0];
+            int ie = c[1];
+            int ps = c[2];
+            int pe = c[3];
 
             int i = is;
             while (i <= ie) {
-                if (inorder[i] == root) {
+                if (inorder[i] == postorder[pe]) {
                     break;
                 }
                 i++;
             }
-
-            TreeNode left = buildTree(is, i - 1, ps, ps + i - 1 - is);
-
-            TreeNode right = buildTree(i + 1, ie, ps + i - is, pe - 1);
-
-            return new TreeNode(root, left, right);
+            // 0 代表左节点，1代表右节点
+            if (ps <= ps + i - 1 - is) cs.push(new int[]{is, i - 1, ps, ps + i - 1 - is, 0});
+            if (ps + i - is <= pe - 1) cs.push(new int[]{i + 1, ie, ps + i - is, pe - 1, 1});
         }
 
     }
