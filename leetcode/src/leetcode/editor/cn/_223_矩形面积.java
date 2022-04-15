7176414f666a9fe94caea8a8e092eef715e60908
caiwillie <caiwl@shinemo.com>
2022-04-15 17:47:27 +0800

    更新代码

--- leetcode/src/leetcode/editor/cn/_223_矩形面积.java
+++ leetcode/src/leetcode/editor/cn/_223_矩形面积.java
@@ -45,7 +45,14 @@ class _223_矩形面积 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-            return 0;
+            int area1 = (ax2 - ax1) * (ay2 - ay1);
+            int area2 = (bx2 - bx1) * (by2 - by1);
+            // 计算重叠宽度和高度，如果为负数，就说明不重叠
+            int overlapWidth = Math.min(ax2, bx2) - Math.max(ax1, bx1);
+            int overlapHeight = Math.min(ay2, by2) - Math.max(ay1, by1);
+            // 不重叠的部分面积计算为0
+            int overlapArea = Math.max(overlapWidth, 0) * Math.max(overlapHeight, 0);
+            return area1 + area2 - overlapArea;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
